d02a959fca3ef91bedb42be28a76c462681768f3
tw-bert <tw-bert@users.noreply.github.com>
2014-02-24 14:09:21 +0100

    _plan: mx_TOOLING_sessioninfo had verkeerde naam en verkeerde namespace. Verhuisd naar sc_mspython_threading.

---
+++ src/panaedra/msroot/mspy/logic/sc_mspython_threading.py
@@ -0,0 +1,65 @@
+import threading
+
+from copy import deepcopy
+
+class sc_mspython_threading:
+  """Get _TERM_UNDISCLOSED_/threading session information from the python (sub-)environment. Also inter-thread control."""
+  
+  tThreadInfo = {}
+  tRegisteredThreads = []
+  oLock = threading.RLock()
+
+  @classmethod
+  def PyRegisterThread(cls, oEventIP):
+    """Called from Python only"""
+    with cls.oLock:
+      if not oEventIP in cls.tRegisteredThreads:
+        cls.tRegisteredThreads += (oEventIP,)
+  
+  @classmethod
+  def PyUnregisterThread(cls, oEventIP):
+    """Called from Python only"""
+    with cls.oLock:
+      cls.tRegisteredThreads.remove(oEventIP)
+  
+  @classmethod
+  def GetThreadInfo(cls,cDataIP):
+    """Called from Bridge"""
+    with cls.oLock:
+      tThreadInfoCpy = deepcopy(cls.tThreadInfo)
+    tRet = []
+    for (a,b) in tThreadInfoCpy.iteritems():
+      tRet += ('[%s] %s' % (a,b),)
+    return '\n'.join(tRet)
+  
+  @classmethod
+  def ClearThreadInfo(cls,cDataIP):
+    """Called from Bridge"""
+    with cls.oLock:
+      cls.tThreadInfo = {}
+    return ''
+
+  @classmethod
+  def GetNextThreadEndLock(cls):
+    """Called from Python only"""
+    oThreadEndLockOP = None  
+    with cls.oLock:
+      if len(cls.tRegisteredThreads) > 0:
+        oThreadEndLockOP = cls.tRegisteredThreads[0]
+    return oThreadEndLockOP
+
+  @classmethod
+  def WaitAllThreadsCompleted(cls,cDataIP):
+    """Called from Bridge"""
+    cRet = ''
+    iThreadCount = threading.active_count() # Including main thread
+    if iThreadCount > 1:
+      cRet += 'Active thread count: %s\n' % iThreadCount
+    oThreadEndLock = cls.GetNextThreadEndLock()
+    while not oThreadEndLock is None:
+      cRet += 'Waiting on thread: %s\n' % repr(oThreadEndLock)
+      oThreadEndLock.wait()
+      oThreadEndLock = cls.GetNextThreadEndLock()
+    return cRet
+
+#EOF
