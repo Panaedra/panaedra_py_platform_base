d706e06b8859b3e142c8e616e04ebf39d3775b2b
tw-bert <tw-bert@users.noreply.github.com>
2014-05-15 17:22:45 +0200

    _plan: [exporteren Qx Definitions naar _CLOUD_] c_mxdyn__3DP__qx_qxdef_export_to__CLOUD__xu.py : bezig met uitlezen via lxml en xpath

--- src/panaedra/msroot/msutil/logic/sc_msxml_lxml.py
+++ src/panaedra/msroot/msutil/logic/sc_msxml_lxml.py
@@ -9,8 +9,8 @@ class sc_msxml_lxml(object):
   
   oDefaultParser = None
   
-  @staticmethod
-  def GetFormattedXml(oEtreeIP, oRawTransformCallback=None):
+  @classmethod
+  def GetFormattedXml(cls, oEtreeIP, oRawTransformCallback=None):
     """
     Call etree.tostring, standardized to avoid binary differences in generated/committed xml files
     """
@@ -28,8 +28,8 @@ class sc_msxml_lxml(object):
     oEtree = etree.fromstring(cXmlIP, cls.oDefaultParser)
     return oEtree
 
-  @staticmethod
-  def ChildnodesToDict(oEtreeIP, cXpathRootIP, cChildnodenameIP, cXpathChildKeyIP, tNsIP):
+  @classmethod
+  def ChildnodesToDict(cls, oEtreeIP, cXpathRootIP, cChildnodenameIP, cXpathChildKeyIP, tNsIP):
     """
     Create a dictionary by iteration the children of a node, and creating a key by 'text' of the supplied xpath.
     The python built-in Ellipsis object is used as a key to store the root node.
@@ -73,8 +73,8 @@ class sc_msxml_lxml(object):
           
     return tRet
 
-  @staticmethod
-  def CompactNamespace(oEtreeIP):
+  @classmethod
+  def CompactNamespace(cls, oEtreeIP):
     tag = oEtreeIP.tag
     for ns in oEtreeIP.nsmap:
       prefix = "{"+oEtreeIP.nsmap[ns]+"}"
@@ -82,8 +82,8 @@ class sc_msxml_lxml(object):
         return ns+":"+tag[len(prefix):]
     return tag
 
-  @staticmethod
-  def FullNamespaceByTree(oEtreeIP, cTagIOP):
+  @classmethod
+  def FullNamespaceByTree(cls, oEtreeIP, cTagIOP):
     # Note: only no-namespace for now, adjust code when needed
     if cTagIOP.find(':') == -1 and cTagIOP.find('{') == -1 :
       if oEtreeIP.nsmap.has_key(None):
@@ -92,8 +92,8 @@ class sc_msxml_lxml(object):
           cTagIOP = '{%s}%s' % (cNs,cTagIOP)
     return cTagIOP
   
-  @staticmethod
-  def FullNamespaceByDict(tDictIP, cTagIOP):
+  @classmethod
+  def FullNamespaceByDict(cls, tDictIP, cTagIOP):
     # Note: only no-namespace for now, adjust code when needed
     if cTagIOP.find(':') == -1 and cTagIOP.find('{') == -1 :
       if tDictIP.has_key(None):
