fff60367a5ede632394ca83546398320241414c9
tw-bert <tw-bert@users.noreply.github.com>
2014-08-08 14:30:40 +0200

    _plan: mantis@1798 [_TOOLING_->Lua] (_TOOLING_: improve logging)

--- src/panaedra/msroot/msutil/logic/sc_date_timestamp.py
+++ src/panaedra/msroot/msutil/logic/sc_date_timestamp.py
@@ -10,33 +10,4 @@ class sc_date_timestamp(object):
   def cTimeOnly_Short_WithPeriods(cls):
     return '%s' % datetime.now().strftime('%H:%M:%S')
 
-
-# Notes:
-#
-# http://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
-# %a  Locale’s abbreviated weekday name.
-# %A  Locale’s full weekday name.      
-# %b  Locale’s abbreviated month name.     
-# %B  Locale’s full month name.
-# %c  Locale’s appropriate date and time representation.   
-# %d  Day of the month as a decimal number [01,31].    
-# %f  Microsecond as a decimal number [0,999999], zero-padded on the left
-# %H  Hour (24-hour clock) as a decimal number [00,23].    
-# %I  Hour (12-hour clock) as a decimal number [01,12].    
-# %j  Day of the year as a decimal number [001,366].   
-# %m  Month as a decimal number [01,12].   
-# %M  Minute as a decimal number [00,59].      
-# %p  Locale’s equivalent of either AM or PM.
-# %S  Second as a decimal number [00,61].
-# %U  Week number of the year (Sunday as the first day of the week)
-# %w  Weekday as a decimal number [0(Sunday),6].   
-# %W  Week number of the year (Monday as the first day of the week)
-# %x  Locale’s appropriate date representation.    
-# %X  Locale’s appropriate time representation.    
-# %y  Year without century as a decimal number [00,99].    
-# %Y  Year with century as a decimal number.   
-# %z  UTC offset in the form +HHMM or -HHMM.
-# %Z  Time zone name (empty string if the object is _CCMPNY_ve).    
-# %%  A literal '%' character.
-
 #EOF
--- src/panaedra/msroot/msutil/logic/sc_log.py
+++ src/panaedra/msroot/msutil/logic/sc_log.py
@@ -1,5 +1,6 @@
-from panaedra.msroot.msutil.logic.sc_path import sc_path
 import os.path
+
+from panaedra.msroot.msutil.logic.sc_path import sc_path
 from panaedra.msroot.msflux.logic_xu.sc_msflux_shutdownrequest_xu import sc_environment
 from panaedra.msroot.msutil.logic.sc_date_timestamp import sc_date_timestamp
 
@@ -7,8 +8,8 @@ class sc_log(object):
   '''Factory class
   '''
   @classmethod
-  def GetLogger(cLognameIP, cSublogdirIP=None):
-    oRet = c_mslog(cLognameIP, cSublogdirIP)
+  def GetLogger(cls, cSourcenameIP, cLognameIP, cSublogdirIP=None):
+    oRet = c_mslog(cSourcenameIP, cLognameIP, cSublogdirIP)
     return oRet
 
 class c_mslog(object):
@@ -27,26 +28,26 @@ class c_mslog(object):
     
     if False:
       '''BoB: Pydev type hinting, epydoc, sphinx or assert implementations not working. 2014Q3'''
-      self.cSourcename = '' 
-      self.cLogname = ''
-      self.cSublogdir = ''
+      self.cSourcename               = '' 
+      self.cLogname                  = ''
+      self.cSublogdir                = ''
       self._cSnapshotLogFileNamePrev = '' 
-      self._cSnapshotSuffix = ''   
+      self._cSnapshotSuffix          = ''   
       '''EoB: Pydev type hinting, epydoc, sphinx or assert implementations not working. 2014Q3''' 
       
   def cSnapshotLogFileName(self):
-    tSnapshotLogFileName = []
-    aPnd = tSnapshotLogFileName.append
-    if self.cSublogdirIP is None:
-      aPnd(sc_path.cLogverboseDir)
-    else:  
-      aPnd(os.path.join(sc_path.cLogverboseDir, self.cSublogdirIP))
-    aPnd(self.cSourcename)
-    aPnd('_{0}_{1}_{2}snapshot.log'.format(
-      sc_environment.cEnv.lower(),
-      sc_date_timestamp.cTimeStamp_Short_Date(),
-      self._cSnapshotSuffix.lower() + '_' if not self._cSnapshotSuffix is None and len(self._cSnapshotSuffix) > 0 else ''))
-    cRet = ''.join(tSnapshotLogFileName)
+    
+    cRet = '{0[dir]}/{0[subdir]}{0[src]}_{0[env]}_{0[stamp]}_{0[sfx]}snapshot.log'.format(
+      {'dir'         : sc_path.cLogverboseDir,
+       'subdir'      : '' if (self.cSublogdir is None or len(self.cSublogdir) == 0) else self.cSublogdir + '/',
+       'src'         : self.cSourcename,
+       'env'         : sc_environment.cEnv.lower(),
+       'stamp'       : sc_date_timestamp.cTimeStamp_Short_Date(),
+       'sfx'         : '' if (self._cSnapshotSuffix is None or len(self._cSnapshotSuffix) == 0) else self._cSnapshotSuffix.lower() + '_',
+       })
+     
+    cRet = os.path.normpath(cRet)
+    
     if self._cSnapshotLogFileNamePrev <> cRet:
       self._cSnapshotLogFileNamePrev = cRet
       with open(cRet,'ab') as f:
@@ -58,6 +59,7 @@ class c_mslog(object):
     ''' Write a snapshot file. 
        The separate snapshot file will be overwritten with each call.'''
     self._cSnapshotSuffix = cSuffixIP
+    
     ''' write message to snapshot log '''
     with open(self.cSnapshotLogFileName(),'wb') as f:
       if self.bIncludeTimeStamp:
