d1daba14bfefba41642a6497354e20085f990198
tw-bert <tw-bert@users.noreply.github.com>
2014-08-13 17:33:10 +0200

    _plan: mantis@1798 [_TOOLING_->Lua] (_TOOLING_: retry after TimeoutError)

--- src/panaedra/msroot/msutil/logic/c_mshelper_numretries.py
+++ src/panaedra/msroot/msutil/logic/c_mshelper_numretries.py
@@ -18,17 +18,6 @@ class c_mshelper_numretries(object):
      
   '''
   
-  @staticmethod
-  def _decorator(*args,**kwargs):
-    _args=args
-    def wrap(*__args):
-      if len(__args) == 0: __args=_args
-      elif len(_args) > 0 and not hasattr(__args[0], '__call__'): 
-        __args=_args
-        kwargs['callobject'] = __args[0]
-      return c_mshelper_numretries(*__args,**kwargs).execute()
-    return wrap
-  
   def __init__(self,  
     func=None, callobject=None, func_args=(), func_kwargs={}, 
     max_tries=5, retry_seconds=[0.3, 1.0, 5.0], 
@@ -122,7 +111,54 @@ class c_mshelper_numretries(object):
       if not aRetry is None:
         aRetry(*self.tRetryArgs, **self.tRetryKwArgs)
 
-# to be used as decorator:
-num_retries=c_mshelper_numretries._decorator
+_DEBUG_DECORATOR=False
+
+class num_retries(object):
+  '''
+  Decorator. Works for functions as well as methods.
+  
+  Parenthesis are mandatory, like this: 
+    @num_retries()
+  You can use any parameters of the c_mshelper_numretries constructor, like this: 
+    @num_retries(max_tries=4,retry_seconds=[1.0,2.0],exc_types=[TimeoutError])
+  '''
+  
+  def __init__(self, *iargs, **ikwargs):
+    '''If there are decorator arguments, the function
+       to be decorated is *not* passed to the constructor.'''
+    self._iargs = iargs
+    self._ikwargs = ikwargs
+    self._cargs = None
+    self._fargs = None
+    if len(iargs) > 0:
+      '''If parameters are omitted, no object pointer is passed in case of object methods. 
+         To avoid confusion, we make parenthesis mandatory, even for non-OO.'''
+      raise ImportError('This decorator requires parameters, like this: @num_retries()')
+    if _DEBUG_DECORATOR:
+      sys.stderr.write('INIT phase: %s %s\n' % (repr(self._iargs),repr(self._ikwargs)))
+
+  def __call__(self, *cargs):
+    '''If there are decorator arguments, __call__() is only
+       called once, as part of the decoration process.'''
+    self._cargs = cargs
+    if _DEBUG_DECORATOR:
+      sys.stderr.write('SETUP phase %s %s %s %s\n' % (repr(self._iargs),repr(self._ikwargs),repr(self._cargs),repr(self._fargs)))
+    def wrapped_f(*fargs):
+      self._fargs =  fargs
+      if _DEBUG_DECORATOR:
+        sys.stderr.write('WRAP phase: %s %s %s %s\n' % (repr(self._iargs),repr(self._ikwargs),repr(self._cargs),repr(self._fargs)))
+      if len(self._cargs) >= 1:
+        if _DEBUG_DECORATOR:
+          sys.stderr.write('RUN phase: start.\n')
+        self.runphase()  
+        if _DEBUG_DECORATOR:
+          sys.stderr.write('RUN phase: done.\n')
+    return wrapped_f
+
+  def runphase(self, *cargs):
+    oNumRetries = c_mshelper_numretries(*self._cargs, **self._ikwargs)
+    if len(self._fargs) > 0:
+      oNumRetries.oCallSelf = self._fargs[0] 
+    oNumRetries.execute()
 
 #EOF
