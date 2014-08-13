6f126ea65c9d1bd9a2c9a395764d1bc5b077d2f0
tw-bert <tw-bert@users.noreply.github.com>
2014-08-13 14:08:12 +0200

    _plan: mantis@1798 [_TOOLING_->Lua] (_TOOLING_: retry after TimeoutError)

--- src/panaedra/msroot/msutil/logic/c_mshelper_numretries.py
+++ src/panaedra/msroot/msutil/logic/c_mshelper_numretries.py
@@ -1,5 +1,8 @@
 import os, sys
 from panaedra.msroot.msutil.logic.sc_mspysys import sc_mspysys
+from copy import deepcopy
+from panaedra.msroot.msutil.logic.sc_date_timestamp import sc_date_timestamp
+import json
 
 class c_mshelper_numretries(object):
   '''Helper object. Retry a certain action a fixed number of times. 
@@ -17,36 +20,39 @@ class c_mshelper_numretries(object):
   
   @staticmethod
   def _decorator(*args,**kwargs):
-    def wrap(func):
-      kwargs['func']=func
-      return c_mshelper_numretries(**kwargs).execute()
+    _args=args
+    def wrap(*__args):
+      if len(__args) == 0: __args=_args
+      return c_mshelper_numretries(*__args,**kwargs).execute()
     return wrap
   
   def __init__(self, 
     func=None, func_args=(), func_kwargs={}, 
-    max_tries=5, retry_seconds=[0.3, 1.0, 2.5, 5.0], 
-    exc_types=[TypeError,ValueError], 
+    max_tries=5, retry_seconds=[0.3, 1.0, 5.0], 
+    exc_types=[ZeroDivisionError], stderr_feedback=True,
     retry_func=[None], retry_args=(), retry_kwargs={}):
     ''' Constructor
     
-    :param    func:           The function to be called
-    :type     func:           function 
-    :param    func_args:      list of arguments for the function to be called
-    :type     func_args:      list 
-    :param    func_kwargs:    dict of keyword arguments for the function to be called
-    :type     func_kwargs:    dict 
-    :param    max_tries:      An exception is raised after max_tries is reached. max_tries=1 does not retry. max_tries=0 does nothing at all.
-    :type     max_tries:      int 
-    :param    retry_seconds:  Can be 1 float, or a list of floats. I.E.: [0.5, 1.0, 5.0] will wait 0.5 seconds after the first fail, 1.0 seconds after the second fail, 5.0 seconds after fail three to N. 
-    :type     retry_seconds:  list 
-    :param    exc_types:      If any thrown exception is not in this list, no retries will be done and the exception will be raised as normal. If the list is [Exception], any exception will be retried. 
-    :type     exc_types:      list 
-    :param    retry_func:     Can be 1 function pointer, or a list of function pointers. Entries with None are handled as no-ops. Called if except, post-pause. len(list) should be one less than max_retries, rest is ignored. I.E.: [onfirst, onrest] will execute onfirst on the first iteration, and onrest on all other iterations 
-    :type     retry_func:     list 
-    :param    retry_args:     list of arguments for all (!) retry functions
-    :type     retry_args:     list 
-    :param    retry_kwargs:   dict of keyword arguments for all (!) retry functions
-    :type     retry_kwargs:   dict 
+    :param    func:            The function to be called
+    :type     func:            function 
+    :param    func_args:       list of arguments for the function to be called
+    :type     func_args:       list 
+    :param    func_kwargs:     dict of keyword arguments for the function to be called
+    :type     func_kwargs:     dict 
+    :param    stderr_feedback: Set to False if no feedback report is to be written to stderr on final fail/raise
+    :type     stderr_feedback: bool
+    :param    max_tries:       An exception is raised after max_tries is reached. max_tries=1 does not retry. max_tries=0 does nothing at all.
+    :type     max_tries:       int 
+    :param    retry_seconds:   Can be 1 float, or a list of floats. I.E.: [0.5, 1.0, 5.0] will wait 0.5 seconds after the first fail, 1.0 seconds after the second fail, 5.0 seconds after fail three to N. 
+    :type     retry_seconds:   list 
+    :param    exc_types:       If any thrown exception is not in this list, no retries will be done and the exception will be raised as normal. If the list is [Exception], any exception will be retried. 
+    :type     exc_types:       list 
+    :param    retry_func:      Can be 1 function pointer, or a list of function pointers. Entries with None are handled as no-ops. Called if except, post-pause. len(list) should be one less than max_retries, rest is ignored. I.E.: [onfirst, onrest] will execute onfirst on the first iteration, and onrest on all other iterations 
+    :type     retry_func:      list 
+    :param    retry_args:      list of arguments for all (!) retry functions
+    :type     retry_args:      list 
+    :param    retry_kwargs:    dict of keyword arguments for all (!) retry functions
+    :type     retry_kwargs:    dict 
     
     :rtype:   c_mshelper_numretries
     :returns: a reusable object for retry loops
@@ -60,6 +66,8 @@ class c_mshelper_numretries(object):
     self.tRetryFunc=retry_func
     self.tRetryArgs=retry_args
     self.tRetryKwArgs=retry_kwargs
+    self.bStderrFeedback=stderr_feedback
+    self.tFeedback=[]
     
     # To avoid lingering circular references at exceptions within exceptions,
     # local variables should not be used. We use data members here for all 
@@ -82,10 +90,18 @@ class c_mshelper_numretries(object):
           del(self._nolingr_oExcType, self._nolingr_oExcObj, self._nolingr_oExcTb)
           raise
         fname = os.path.split(self._nolingr_oExcTb.tb_frame.f_code.co_filename)[1]
-        print iRetry, repr(self._nolingr_oExcType), fname, self._nolingr_oExcTb.tb_lineno
+        if self.bStderrFeedback:
+          self.tFeedback.append((
+            sc_date_timestamp.cTimeOnly_Long_WithPeriods(),
+            deepcopy(self._nolingr_oExcType.__name__), 
+            "{0}:{1}".format(fname,self._nolingr_oExcTb.tb_lineno),))
         # Avoid lingering circular references
         del(self._nolingr_oExcType, self._nolingr_oExcObj, self._nolingr_oExcTb)
-        if iRetry == self.iMaxTries: 
+        if iRetry == self.iMaxTries:
+          if self.bStderrFeedback:
+            sys.stderr.write(json.dumps(self.tFeedback,indent=2))
+            sys.stderr.write('\n')
+            sys.stderr.flush()
           raise
       fPause = self.tRetrySeconds if not hasattr(self.tRetrySeconds, '__iter__') else self.tRetrySeconds[min(len(self.tRetrySeconds), iRetry) - 1]  
       sc_mspysys.Sleep(fPause)
