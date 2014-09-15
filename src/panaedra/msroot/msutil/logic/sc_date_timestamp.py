6ae88388d6a7a23619be6882de0e5a558d83421f
tw-bert <tw-bert@users.noreply.github.com>
2014-09-15 14:55:21 +0200

    _plan: mantis@1798 [_TOOLING_->Lua] (_TOOLING_: improve logging)

--- src/panaedra/msroot/msutil/logic/sc_date_timestamp.py
+++ src/panaedra/msroot/msutil/logic/sc_date_timestamp.py
@@ -5,6 +5,10 @@ class sc_date_timestamp(object):
   @classmethod  
   def cTimeStamp_Short_Date(cls):
     return '%s' % datetime.now().strftime('%Y%m%d')
+ 
+  @classmethod  
+  def cTimeStamp_Short_DatePerMinute(cls):
+    return '%s' % datetime.now().strftime('%Y%m%d%H%M')
 
   @classmethod  
   def cTimeOnly_Short_WithPeriods(cls):
@@ -13,5 +17,6 @@ class sc_date_timestamp(object):
   @classmethod  
   def cTimeOnly_Long_WithPeriods(cls):
     return '%s' % datetime.now().strftime('%H:%M:%S.%f')
+  
 
 #EOF
