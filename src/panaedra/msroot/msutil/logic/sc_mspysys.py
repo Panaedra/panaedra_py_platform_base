426da625b141dfcdbf6127b3ffcfa2bc849792d1
tw-bert <tw-bert@users.noreply.github.com>
2014-09-22 13:45:20 +0200

    _plan: mantis@1798 [_TOOLING_->Lua] (_TOOLING_: bugfix CORE dump _TOOLING_ at end of ABL session; actual _TOOLING_ batch as well as unittests)

--- src/panaedra/msroot/msutil/logic/sc_mspysys.py
+++ src/panaedra/msroot/msutil/logic/sc_mspysys.py
@@ -1,3 +1,6 @@
+import gc
+import sys
+import inspect
 import threading
 
 class sc_mspysys(object):
@@ -18,5 +21,71 @@ class sc_mspysys(object):
     dummy_event = threading.Event()
     dummy_event.wait(timeout=seconds)
     return ''
+  
+  @classmethod
+  def AllObjectsToTrash(cls, cDataIP):
+    '''Called by bridge. 
+       Does a 'del' on all class objects. 
+       Prevents core dumps at the end of an ABL session. 
+       For manual investigation: use DumpAllObjects()
+    '''
+    tObjects=gc.get_objects()
+    for o in tObjects:
+      if getattr(o, "__class__", None):
+        del o
+    return ''
+
+  @classmethod
+  def DumpAllObjects(cls,tFindFilter=[],oStream=None):
+    '''For example, call:
+       # with open('/mydir/mydump.log','wb') as f:
+       #   sc_mspysys.DumpAllObjects(tFindFilter=['edis','sc_', 'c_clients', 'Connection', 'Lock', 'Thread', 'Queue', 'Event',],oStream=f)
+    '''
+    exclude = [
+        "function",
+        "type",
+        "list",
+        "dict",
+        "tuple",
+        "wrapper_descriptor",
+        "module",
+        "method_descriptor",
+        "member_descriptor",
+        "instancemethod",
+        "builtin_function_or_method",
+        "frame",
+        "classmethod",
+        "classmethod_descriptor",
+        "_Environ",
+        "MemoryError",
+        "_Printer",
+        "_Helper",
+        "getset_descriptor",
+        ]
+    gc.collect()
+    oo = gc.get_objects()
+    if oStream is None: oStream = sys.stdout
+    for o in oo:
+      try:
+        if getattr(o, "__class__", None):
+          name = o.__class__.__name__
+          if name not in exclude:
+            filename = ''
+            try:
+              filename = inspect.getabsfile(o.__class__)
+            except:            
+              filename = repr(o.__class__)
+            bFiltered=False
+            if len(tFindFilter) > 0:
+              bFiltered=True
+              for item in tFindFilter:
+                if name.find(item) >= 0:
+                  bFiltered=False
+                  break 
+            if not bFiltered:
+              oStream.write("Object of class: {0} ... defined in file: {1}\n".format(name,filename))
+              oStream.flush()
+      except:
+        pass         
     
 #EOF
