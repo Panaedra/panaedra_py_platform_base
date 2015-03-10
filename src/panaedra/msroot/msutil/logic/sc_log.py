d8771af0950fe0079482b880e3122fd15a329a35
anon_705 <tw-bert@users.noreply.github.com>
2015-03-10 16:44:53 +0100

    _plan: mantis@1986 [Een wizard voor het genereren van de benodigheden voor het maken van nieuwe _TOOLING_] (GetLoggerPostponed method toegevoegd, -EN interface definities i_log.cls uit progress amdir_gui toegevoegd)

--- src/panaedra/msroot/msutil/logic/sc_log.py
+++ src/panaedra/msroot/msutil/logic/sc_log.py
@@ -3,6 +3,7 @@ import os.path
 from panaedra.msroot.msutil.logic.sc_path import sc_path
 from panaedra.msroot.msflux.logic_xu.sc_msflux_shutdownrequest_xu import sc_environment
 from panaedra.msroot.msutil.logic.sc_date_timestamp import sc_date_timestamp
+from panaedra.msroot.msutil.logic.c_log_postponed import c_log_postponed
 
 class sc_log(object):
   '''Factory class
@@ -11,6 +12,12 @@ class sc_log(object):
   def GetLogger(cls, cLognameIP, cSublogdirIP=None):
     oRet = c_mslog(cLognameIP, cSublogdirIP)
     return oRet
+  
+  @classmethod
+  def GetLoggerPostponed(cls):
+    '''get a (new) logging object'''
+    return c_log_postponed()
+
 
 class c_mslog(object):
   '''A log object that logs to file. From a bridge (especially from a fifo thread), use snapshot logfiles only.
@@ -24,6 +31,24 @@ class c_mslog(object):
     self._cSnapshotLogFileNamePrev = None 
     self._cSnapshotSuffix = None
     self.bIncludeTimeStamp = True
+      
+    '''Set this property to add an own header to the status log, 
+       the normal header will be placed underneath this one'''
+    self.cOwnHeader = None
+    '''direct errors to the log-manager '''
+    self.bErrorOnLogManager = None 
+    '''error log file name'''
+    self.cErrorLogFileName  = None 
+    '''status log file name'''
+    self.cStatusLogFileName = None 
+    '''timestamp toevoegen aan WriteStatusLn regels'''
+    self.bIncludeTimeStamp = None 
+    ''' timestamp'''
+    self.cTimeStamp = None 
+    '''for WriteStatusLnBuf calls, automatically do a WriteStatusHeader if 
+    * something else was written in between. Also at day switch.'''
+    self.bAutoHeaders = None 
+    
     
     if False:
       '''BoB: Pydev type hinting, epydoc, sphinx or assert implementations not working. 2014Q3'''
@@ -63,5 +88,63 @@ class c_mslog(object):
       if self.bIncludeTimeStamp:
         f.write('{0:<20}'.format(sc_date_timestamp.cTimeOnly_Long_WithPeriods()))
       f.write('{0}\n'.format(cMessageIP))
+
+    
+  def WriteStatus(self, cMessageIP): 
+    pass
+  
+  
+  def WriteStatusEmptyLn(self): 
+    pass 
+    
+  
+  def WriteStatusInit(self, hBufferReportIP): 
+    '''formatted with columns with buffer/query , field-handles possible''' 
+    pass 
+  
+  
+  def WriteStatusHeader(self, hBufferReportIP): 
+    pass 
+  
+  
+  def WriteStatusLnBuf(self, hBufferReportIP): 
+    pass 
+  
+  
+ 
+  def SnapshotStatusLnBuf(self, cSuffixIP):
+    '''write a snapshot file with header. the separate snapshot file will be overwritten with each call'''
+    pass 
+  
+  
+  def OpenStatusStream(self):
+    pass
+  
+
+  def CloseStatusStream(self): 
+    pass 
+  
+  
+  # Write error messages 
+  def WriteError(self, cMessage):
+    pass 
+
+  
+  def WriteErrorLn(self, cMessage):
+    pass 
+  
+  
+  def WriteException(self, oExepctionIP, cExtraTextIP): 
+    pass 
+    
+  
+  def WriteMissingData(self, hBufferIP, cSvKeyValuesIP): 
+    pass 
+  
+ 
+  def Dispose(self): 
+    '''signal the end of the logging'''
+    pass 
+
 
 #EOF
