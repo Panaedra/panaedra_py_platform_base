b1acc6c8581f122dc4f0ca59182d60eea5c6f7b4
anon_705 <tw-bert@users.noreply.github.com>
2015-03-15 15:45:42 +0100

    _plan: mantis@1992 [AOF Backup _TOOLING_] (get file section as string method toegevoegd)

--- src/panaedra/msroot/msutil/logic/sc_msosfile.py
+++ src/panaedra/msroot/msutil/logic/sc_msosfile.py
@@ -4,9 +4,30 @@ class sc_msosfile(object):
   @classmethod
   def WriteStringToFile(cls, cFilepathIP, cStringIP):      
     
-    with open(cFilepathIP, "w") as strFile:
+    with open(cFilepathIP, 'w') as strFile:
       strFile.write(cStringIP)
       strFile.close
+      
+
+  @classmethod  
+  def GetFileSectionAsString(cls, cInputFilepathIP, iStartBytePositionIP, iFilePortionExtentIP):   
+   
+    with open(cInputFilepathIP, 'rb') as oInputFile:
+      oInputFile.seek(iStartBytePositionIP, 0)
+      return oInputFile.read(iFilePortionExtentIP)
+
+  @classmethod 
+  def CopyFileSection(cls, cInputFilepathIP, cOutputFilepathIP, iStartBytePositionIP, iFilePortionExtentIP): 
+
+    with open(cInputFilepathIP, 'rb') as oInputFile:
+      oInputFile.seek(iStartBytePositionIP, 0)
+      with open(cOutputFilepathIP, 'wb') as oOutputFile:
+        oOutputFile.write(oInputFile.read(iFilePortionExtentIP))
+
+
+if __name__ == '__main__':
+  
+  sc_msosfile.CopyFileSection('E:/_CLOUD_test/aof_filesize_nonlive_20150313_.log', 'E:/_CLOUD_test/test.log', 1000, 2000)
 
 #EOF
 
