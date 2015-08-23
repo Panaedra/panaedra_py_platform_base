eb57c759e82cd72f20f35673d34cbea905b5fbc6
tw-bert <tw-bert@users.noreply.github.com>
2015-08-23 14:51:12 +0200

    _plan: Via python XlsxWriter, create an Excel file with charts of _DATA_ _TOOLING_dump hq-timestamp output file executed on srv-benchmark-aix

--- src/panaedra/msroot/msutil/logic/sc_mshqtimestamp_excel_logic.py
+++ src/panaedra/msroot/msutil/logic/sc_mshqtimestamp_excel_logic.py
@@ -16,8 +16,23 @@ class sc_mshqtimestamp_excel_logic(object):
     oFixedfont.set_font_name('Consolas')
     oFixedfont.set_font_size(10)
     
-    tHeadings = ['Line', 'Time', 'ProcID', 'Proc', 'Delta', 'LoopStart', 'LoopDelta', 'Var']  
-    tData=[[],[],[],[],[],[],[],[]]
+    class sHeading(object):
+      tHeadings = ['Line', 'Time', 'ProcID', 'Proc', 'Delta', 'LoopStart', 'LoopDelta', 'Var', 'LoopNo', 'LoopAt', 'LoopDeltaX']
+      Line       = 0 
+      Time       = 1
+      ProcID     = 2
+      Proc       = 3
+      Delta      = 4
+      LoopStart  = 5
+      LoopDelta  = 6
+      Var        = 7 
+      LoopNo     = 8 
+      LoopAt     = 9
+      LoopDeltaX = 10
+    
+    tData=[]
+    for i in range(len(sHeading.tHeadings)):  
+      tData.append([])
     fFirstTime=None
     iLoopProcID=None
     tProcID={}
@@ -26,14 +41,14 @@ class sc_mshqtimestamp_excel_logic(object):
     with open(cFileIP) as f:
       for i,cLine in enumerate(f):
         iTotalLines+=1
-        tData[0].append(i)
+        tData[sHeading.Line].append(i)
         cTime,cRemainder=cLine.rstrip().split(':')
         cRemainder=cRemainder.lstrip()
         fTime=float(cTime)
         if fFirstTime is None: fFirstTime=fTime
         fTime-=fFirstTime
         cProc,cProcseq,cVar=cRemainder.split('\x03')
-        tData[1].append(fTime)
+        tData[sHeading.Time].append(fTime)
         if not (cProc,cProcseq) in tProcID.keys():
           tProcID[(cProc,cProcseq)]=[iProcID,i,fTime]
           iProcID+=1
@@ -42,31 +57,37 @@ class sc_mshqtimestamp_excel_logic(object):
             # This is a repetition; indicate as looppoint
             iLoopProcID=tProcID[(cProc,cProcseq)][0]
             # Set first looppoint to True as well
-            tData[5][tProcID[(cProc,cProcseq)][1]]=True
-        tData[2].append(tProcID[(cProc,cProcseq)][0])
-        tData[3].append('{}_{}'.format(cProc,cProcseq))
+            tData[sHeading.LoopStart][tProcID[(cProc,cProcseq)][1]]=True
+        tData[sHeading.ProcID].append(tProcID[(cProc,cProcseq)][0])
+        tData[sHeading.Proc].append('{}_{}'.format(cProc,cProcseq))
         if i==0:
-          tData[4].append(0.0)
+          tData[sHeading.Delta].append(0.0)
         else:
-          fDelta = tData[1][-1] - tData[1][-2]  
-          tData[4].append(fDelta)
+          fDelta = tData[sHeading.Time][-1] - tData[sHeading.Time][-2]  
+          tData[sHeading.Delta].append(fDelta)
         if (not iLoopProcID is None) and (iLoopProcID==tProcID[(cProc,cProcseq)][0]):
-          tData[5].append(True)
+          tData[sHeading.LoopStart].append(True)
         else:
-          tData[5].append(False)
-        tData[6].append(None)  
-        tData[7].append(cVar)
+          tData[sHeading.LoopStart].append(None)
+        tData[sHeading.LoopDelta].append(None)
+        tData[sHeading.Var].append(cVar)
     
     # Calculate the loop delta's
     fTimePrev=None
+    iLoop=0
     for i in range(iTotalLines):
-      if not tData[5][i]:
-        tData[6][i] = 0
+      if not tData[sHeading.LoopStart][i] == True:
+        tData[sHeading.LoopDelta][i] = None
       else:
-        tData[6][i] = 0
+        tData[sHeading.LoopDelta][i] = None
         if fTimePrev!=None:
-          tData[6][i] = tData[1][i] - fTimePrev
-        fTimePrev=tData[1][i]
+          iLoop+=1
+          fLoopDelta=tData[sHeading.Time][i] - fTimePrev
+          tData[sHeading.LoopDelta][i] = fLoopDelta
+          tData[sHeading.LoopNo].append(iLoop)
+          tData[sHeading.LoopAt].append(tData[sHeading.Time][i])
+          tData[sHeading.LoopDeltaX].append(fLoopDelta)
+        fTimePrev=tData[sHeading.Time][i]
     
     oWorksheet.set_column(0,0, 80)  # Column width (of summary)
     cls.AddToSummary(oWorksheet, oFixedfont, 'Delta total: {} seconds'.format(fTime))
@@ -76,27 +97,32 @@ class sc_mshqtimestamp_excel_logic(object):
    
     oNanoFormat = oWorkbook.add_format()
     oNanoFormat.set_num_format('0.000000000')
-    oWorksheet.set_column(iDataStartCol+1,iDataStartCol+1,cell_format=oNanoFormat)
-    oWorksheet.set_column(iDataStartCol+4,iDataStartCol+4,cell_format=oNanoFormat)
     
-    oWorksheet.set_column(iDataStartCol,iDataStartCol, 10)  # Column width
-    oWorksheet.set_column(iDataStartCol+1,iDataStartCol+1, 15)
-    oWorksheet.set_column(iDataStartCol+2,iDataStartCol+2, 10)
-    oWorksheet.set_column(iDataStartCol+3,iDataStartCol+3, 90)
-    oWorksheet.set_column(iDataStartCol+4,iDataStartCol+4, 15)
-    oWorksheet.set_column(iDataStartCol+5,iDataStartCol+5, 10)
-    oWorksheet.set_column(iDataStartCol+6,iDataStartCol+6, 15)
-    oWorksheet.set_column(iDataStartCol+7,iDataStartCol+7, 50)
+    def SetColumn_NanoFormat(iCol):
+      oWorksheet.set_column(iDataStartCol+iCol,iDataStartCol+iCol,cell_format=oNanoFormat)
     
-    oWorksheet.write_row(iDataStartRow-1, iDataStartCol, tHeadings, oBold)
-    oWorksheet.write_column(iDataStartRow, iDataStartCol,  tData[0])
-    oWorksheet.write_column(iDataStartRow, iDataStartCol+1, tData[1])
-    oWorksheet.write_column(iDataStartRow, iDataStartCol+2, tData[2])
-    oWorksheet.write_column(iDataStartRow, iDataStartCol+3, tData[3])
-    oWorksheet.write_column(iDataStartRow, iDataStartCol+4, tData[4])
-    oWorksheet.write_column(iDataStartRow, iDataStartCol+5, tData[5])
-    oWorksheet.write_column(iDataStartRow, iDataStartCol+6, tData[6])
-    oWorksheet.write_column(iDataStartRow, iDataStartCol+7, tData[7])
+    SetColumn_NanoFormat(sHeading.Time);
+    SetColumn_NanoFormat(sHeading.Delta);
+    
+    def SetColumn_Width(iCol, iWidthIP):
+      oWorksheet.set_column(iDataStartCol+iCol,iDataStartCol+iCol, iWidthIP)
+    
+    SetColumn_Width(sHeading.Line, 10)
+    SetColumn_Width(sHeading.Time, 12)
+    SetColumn_Width(sHeading.ProcID, 8)
+    SetColumn_Width(sHeading.Proc, 90)
+    SetColumn_Width(sHeading.Delta, 12)
+    SetColumn_Width(sHeading.LoopStart, 8)
+    SetColumn_Width(sHeading.LoopDelta, 12)
+    SetColumn_Width(sHeading.Var, 50)
+    SetColumn_Width(sHeading.LoopNo, 8)
+    SetColumn_Width(sHeading.LoopAt, 12)
+    SetColumn_Width(sHeading.LoopDeltaX, 12)
+    
+    oWorksheet.write_row(iDataStartRow-1, iDataStartCol, sHeading.tHeadings, oBold)
+    
+    for i in range(len(sHeading.tHeadings)):
+      oWorksheet.write_column(iDataStartRow, iDataStartCol + i,  tData[i])
     
     oWorkbook.close()
   
