19c3357dda4ce3e0595b1c379ec161ff7abcf0db
tw-bert <tw-bert@users.noreply.github.com>
2015-08-23 13:57:04 +0200

    _plan: Via python XlsxWriter, create an Excel file with charts of _DATA_ _TOOLING_dump hq-timestamp output file executed on srv-benchmark-aix

--- src/panaedra/msroot/msutil/logic/sc_mshqtimestamp_excel_logic.py
+++ src/panaedra/msroot/msutil/logic/sc_mshqtimestamp_excel_logic.py
@@ -16,12 +16,16 @@ class sc_mshqtimestamp_excel_logic(object):
     oFixedfont.set_font_name('Consolas')
     oFixedfont.set_font_size(10)
     
-    tHeadings = ['Line', 'Time', 'Proc', 'Var']  
+    tHeadings = ['Line', 'Time', 'ProcID', 'Proc', 'Delta', 'LoopStart', 'LoopDelta', 'Var']  
+    tData=[[],[],[],[],[],[],[],[]]
     fFirstTime=None
-    tData=[[],[],[],[]]
+    iLoopProcID=None
+    tProcID={}
+    iProcID=1
+    iTotalLines=0
     with open(cFileIP) as f:
       for i,cLine in enumerate(f):
-        #print i, cLine.rstrip()
+        iTotalLines+=1
         tData[0].append(i)
         cTime,cRemainder=cLine.rstrip().split(':')
         cRemainder=cRemainder.lstrip()
@@ -30,8 +34,39 @@ class sc_mshqtimestamp_excel_logic(object):
         fTime-=fFirstTime
         cProc,cProcseq,cVar=cRemainder.split('\x03')
         tData[1].append(fTime)
-        tData[2].append('{}_{}'.format(cProc,cProcseq))
-        tData[3].append(cVar)
+        if not (cProc,cProcseq) in tProcID.keys():
+          tProcID[(cProc,cProcseq)]=[iProcID,i,fTime]
+          iProcID+=1
+        else:
+          if iLoopProcID is None:
+            # This is a repetition; indicate as looppoint
+            iLoopProcID=tProcID[(cProc,cProcseq)][0]
+            # Set first looppoint to True as well
+            tData[5][tProcID[(cProc,cProcseq)][1]]=True
+        tData[2].append(tProcID[(cProc,cProcseq)][0])
+        tData[3].append('{}_{}'.format(cProc,cProcseq))
+        if i==0:
+          tData[4].append(0.0)
+        else:
+          fDelta = tData[1][-1] - tData[1][-2]  
+          tData[4].append(fDelta)
+        if (not iLoopProcID is None) and (iLoopProcID==tProcID[(cProc,cProcseq)][0]):
+          tData[5].append(True)
+        else:
+          tData[5].append(False)
+        tData[6].append(None)  
+        tData[7].append(cVar)
+    
+    # Calculate the loop delta's
+    fTimePrev=None
+    for i in range(iTotalLines):
+      if not tData[5][i]:
+        tData[6][i] = 0
+      else:
+        tData[6][i] = 0
+        if fTimePrev!=None:
+          tData[6][i] = tData[1][i] - fTimePrev
+        fTimePrev=tData[1][i]
     
     oWorksheet.set_column(0,0, 80)  # Column width (of summary)
     cls.AddToSummary(oWorksheet, oFixedfont, 'Delta total: {} seconds'.format(fTime))
@@ -39,15 +74,29 @@ class sc_mshqtimestamp_excel_logic(object):
     iDataStartRow=1
     iDataStartCol=1
    
-    oWorksheet.set_column(iDataStartCol,iDataStartCol+1, 20)  # Column width
-    oWorksheet.set_column(iDataStartCol+2,iDataStartCol+2, 90)
-    oWorksheet.set_column(iDataStartCol+3,iDataStartCol+3, 50)
+    oNanoFormat = oWorkbook.add_format()
+    oNanoFormat.set_num_format('0.000000000')
+    oWorksheet.set_column(iDataStartCol+1,iDataStartCol+1,cell_format=oNanoFormat)
+    oWorksheet.set_column(iDataStartCol+4,iDataStartCol+4,cell_format=oNanoFormat)
+    
+    oWorksheet.set_column(iDataStartCol,iDataStartCol, 10)  # Column width
+    oWorksheet.set_column(iDataStartCol+1,iDataStartCol+1, 15)
+    oWorksheet.set_column(iDataStartCol+2,iDataStartCol+2, 10)
+    oWorksheet.set_column(iDataStartCol+3,iDataStartCol+3, 90)
+    oWorksheet.set_column(iDataStartCol+4,iDataStartCol+4, 15)
+    oWorksheet.set_column(iDataStartCol+5,iDataStartCol+5, 10)
+    oWorksheet.set_column(iDataStartCol+6,iDataStartCol+6, 15)
+    oWorksheet.set_column(iDataStartCol+7,iDataStartCol+7, 50)
     
     oWorksheet.write_row(iDataStartRow-1, iDataStartCol, tHeadings, oBold)
     oWorksheet.write_column(iDataStartRow, iDataStartCol,  tData[0])
     oWorksheet.write_column(iDataStartRow, iDataStartCol+1, tData[1])
     oWorksheet.write_column(iDataStartRow, iDataStartCol+2, tData[2])
     oWorksheet.write_column(iDataStartRow, iDataStartCol+3, tData[3])
+    oWorksheet.write_column(iDataStartRow, iDataStartCol+4, tData[4])
+    oWorksheet.write_column(iDataStartRow, iDataStartCol+5, tData[5])
+    oWorksheet.write_column(iDataStartRow, iDataStartCol+6, tData[6])
+    oWorksheet.write_column(iDataStartRow, iDataStartCol+7, tData[7])
     
     oWorkbook.close()
   
