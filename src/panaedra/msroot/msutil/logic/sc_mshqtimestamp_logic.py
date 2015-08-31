cd9768c5cc9844a324c5a38c48da9c08e4db9c64
tw-bert <tw-bert@users.noreply.github.com>
2015-08-31 17:52:58 +0200

    _plan: Hq Timestamps, implementing 'report-comment strings with python literal dict'.

--- src/panaedra/msroot/msutil/logic/sc_mshqtimestamp_logic.py
+++ src/panaedra/msroot/msutil/logic/sc_mshqtimestamp_logic.py
@@ -1,7 +1,8 @@
+import os
 import ast
 import json
+
 from collections import OrderedDict
-import os
 
 class sc_mshqtimestamp_logic(object):
   
@@ -28,7 +29,10 @@ class sc_mshqtimestamp_logic(object):
     
     elif tParam['mode'] == 'PyAppendTimestampsWithDict':
       cls.cself.tRet=OrderedDict()
-      cls.cself.PyAppendTimestampsWithDict(tParam['cHqtFile'])
+      cls.cself.PyAppendTimestampsWithDict(
+        tParam['cHqtFile'], 
+        tParam['dedupe'], 
+        True if not tParam.has_key('replace-file') else tParam['replace-file'])
     
     tRet=cls.cself.tRet
     cls.cself.tRet=None
@@ -38,22 +42,68 @@ class sc_mshqtimestamp_logic(object):
   def __init__(self):
     self.tRet = None
     
-  def PyAppendTimestampsWithDict(self, cHqtFileIP):
+  def PyAppendTimestampsWithDict(self, cHqtFileIP, bDedupeIP, bReplaceFileIP=True):
+    
     tHqtNewFile=os.path.splitext(cHqtFileIP)
     cHqtNewFile=tHqtNewFile[0] + '_new' + tHqtNewFile[1]
-    tDict={}
+    tSourceComments,tDedupeData={},{}
+    
     with open(cHqtFileIP,'rb') as f_in, open(cHqtNewFile,'wb') as f_out:
       for cLine in f_in:
         tLine=cLine.partition(':')
         tLineData=tLine[2].lstrip().split('\x03')
         cSourceLineComment=''
-        if not (tLineData[2], tLineData[3], ) in tDict.keys():
-          with open(tLineData[2],'rb') as f_sourcecode:
-            for i,cSourceLine in enumerate(f_sourcecode):
-              if (i + 1 == tLineData[3]):
-                cSourceLineComment=cSourceLine.partition('/*')[2][0:-4]
-                tDict[(tLineData[2], tLineData[3], )] = cSourceLineComment.strip()
-                break
-        f_out.write('{}{}\n'.format(cLine.rstrip(),cSourceLineComment))
+        if (len(tLineData[3]) == 0     # No line number; just copy line as-is
+            or len(tLineData[6]) > 0): # There is already data (a literal dict) present in this line; just copy line as-is
+          f_out.write('{}{}\n'.format(cLine.rstrip(),cSourceLineComment))
+        else:
+          cRuntimeloc,cLocSequence,cSourcecodeFile,iSourcecodeLine,cVarA,cVarB=tLineData[0],tLineData[1],tLineData[2],int(tLineData[3]),tLineData[4],tLineData[5]
+          if not tSourceComments.has_key(cSourcecodeFile):
+            tSourceComments=self._ParseSourcecode(cSourcecodeFile, tSourceComments)
+          if tSourceComments[cSourcecodeFile].has_key(iSourcecodeLine):
+            cSourceLineComment=tSourceComments[cSourcecodeFile][iSourcecodeLine]
+          if (not bDedupeIP) or (not tDedupeData.has_key((cRuntimeloc,cLocSequence,))):
+            tDedupeData[(cRuntimeloc,cLocSequence,)]=None
+            f_out.write('{}{}\n'.format(cLine.rstrip(),cSourceLineComment))
+          else:
+            f_out.write('{}: {}\x03{}\x03\x03\x03{}\x03{}\x03\n'.format(tLine[0],cRuntimeloc,cLocSequence,cVarA,cVarB))
+    
+    if bReplaceFileIP:
+      tHqtTmpFile=os.path.splitext(cHqtFileIP)
+      cHqtTmpFile=tHqtTmpFile[0] + '_tmp' + tHqtTmpFile[1]
+      if os.path.isfile(cHqtTmpFile):
+        raise RuntimeError('The file "{}" already exists, so rename is unsafe'.format(cHqtTmpFile))
+      os.rename(cHqtFileIP, cHqtTmpFile)
+      if (not os.path.isfile(cHqtTmpFile)) or os.path.isfile(cHqtFileIP):
+        raise RuntimeError('The file "{}" could not be renamed to "{}"'.format(cHqtFileIP, cHqtTmpFile))
+      os.rename(cHqtNewFile, cHqtFileIP)
+      if (not os.path.isfile(cHqtFileIP)) or os.path.isfile(cHqtNewFile):
+        raise RuntimeError('The file "{}" could not be renamed to "{}"'.format(cHqtNewFile, cHqtFileIP))
+      os.remove(cHqtTmpFile)
+      if os.path.isfile(cHqtTmpFile):
+        raise RuntimeError('The file "{}" could not be removed'.format(cHqtTmpFile))
+    
+  def _ParseSourcecode(self, cSourcecodeFileIP, tSourceCommentsIOP):
+    tSourceCommentsIOP[cSourcecodeFileIP]={}
+    with open(cSourcecodeFileIP,'rb') as f_sourcecode:
+      for i,cSourceLine in enumerate(f_sourcecode):
+        if ('{&hq}' in cSourceLine):
+          cSourceLineComment=cSourceLine.partition('/*')[2]
+          cSourceLineComment=cSourceLineComment.rpartition('*/')[0]
+          tSourceCommentsIOP[cSourcecodeFileIP][i+1] = cSourceLineComment.strip()
+    return tSourceCommentsIOP
 
+if __name__ == '__main__':
+  '''
+  cls=sc_mshqtimestamp_logic
+  cls.StaticMain("{'mode':'init',}")
+  cls.StaticMain(
+    # replace-file to False for debugging only.
+    "{'mode':'PyAppendTimestampsWithDict',"
+    "'cHqtFile':'X:/fluxdumpbig__dwan_idetest.txt',"
+    "'dedupe': True,"
+    "'replace-file': False, }"
+    )
+  '''
+  
 #EOF
