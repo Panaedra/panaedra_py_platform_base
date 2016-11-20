8330045bda9e7e8e4e4e1f6996b5aa6e7a7828ae
tw-bert <tw-bert@users.noreply.github.com>
2016-11-20 15:05:15 +0100

    _plan: mantis@2305 [Get _TOOLING_ and _CLOUD_ ready for high availability for production] (c_socketclient , fref added)

--- src/panaedra/msroot/msutil/logic/c_socketclient.py
+++ src/panaedra/msroot/msutil/logic/c_socketclient.py
@@ -7,6 +7,9 @@ import socket
 SOCKET_RECEIVE_BLOCKSIZE = 4096
 
 class c_socketclient(object):
+  '''
+  FREF@bc4c1880d++: A small wrapper for _TERM_UNDISCLOSED_ing with socket connections as a client. Facilitates "PackedMessage" (Prefix each message with a 4-byte length (network byte order, big endian)). Implemented in java (panaedra_javacommon), python (panaedra_python) and python (py-_CLOUD_rollforward). Keep changes to the independent source code locations in sync. 
+  '''
   
   def __init__(self, cHostIP, iPortIP, bBlockingIP = True):
     '''Initialize the TCP/IP socket'''
